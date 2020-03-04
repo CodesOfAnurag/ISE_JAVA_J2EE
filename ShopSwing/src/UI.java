8f5b49f3fbfb8d0eff50f1d3ca7cf09b6da3bcc9
R Anurag Pillai <46356136+CodesOfAnurag@users.noreply.github.com>
2020-03-04 23:55:42 +0530

    Update UI.java

--- ShopSwing/src/UI.java
+++ ShopSwing/src/UI.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
+
 public class UI {
 	private static Component add;
+	
+	Customer cTemp;
 
 	public static void main(String args[]){
 /*		
@@ -33,25 +37,25 @@ public class UI {
 		
 		JOptionPane.showMessageDialog(null, "Successful Login", "Success", JOptionPane.INFORMATION_MESSAGE);
 */
+		
+		HashMap<Integer, Customer> hpno = new HashMap<Integer, Customer>();
+		hpno.put(123456, new Customer(101, 123456, "Anurag"));
+		hpno.put(987654, new Customer(102, 987654, "Anupam"));
+		
 		JFrame inputCustomer = new JFrame("Customer");
 		
 		JPanel cPane = new JPanel();
 		cPane.setLayout(new GridLayout(2,2));
 		
-		JTextField id, name, phone;
-//		id = new JTextField();
-//		name = new JTextField();
-		phone = new JTextField();
+		JTextField phone;
+
+		phone = new JTextField(15);
 		
-//		cPane.add(new JLabel("Customer Id"));
-//		cPane.add(id);
-//		cPane.add(new JLabel("Customer Name"));
-//		cPane.add(name);
 		cPane.add(new JLabel("Customer Phone"));
 		cPane.add(phone);
 		
 		JPanel btnPane = new JPanel();
-		btnPane.setLayout(new GridLayout(3,1));
+		//btnPane.setLayout(new GridLayout(3,1));
 		
 		JButton addB, findB, itemB;
 		addB = new JButton("Add");
@@ -69,7 +73,47 @@ public class UI {
 		inputCustomer.setVisible(true);
 		inputCustomer.setSize(320, 320);
 		inputCustomer.setLayout(new GridLayout(4,1));
-		ssss	
+		
+		JTextArea customerInfo = new JTextArea(150,150);
+		customerInfo.setEnabled(false);
+		inputCustomer.add(customerInfo);
+		
+		
+		addB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				if (hpno.keySet().contains(Integer.parseInt(phone.getText())))
+					JOptionPane.showMessageDialog(null, "Already Exists", "Error", JOptionPane.ERROR_MESSAGE);
+				else {
+					int id = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter ID", "Customer ID", JOptionPane.INFORMATION_MESSAGE));
+					String name = JOptionPane.showInputDialog(null, "Enter Name", "Customer Name", JOptionPane.INFORMATION_MESSAGE);
+					Customer cTemp = new Customer(id, Integer.parseInt(phone.getText()), name);
+					hpno.put(cTemp.phone, cTemp);
+					System.out.println(cTemp);
+					phone.setEnabled(false);
+					addB.setEnabled(false);
+					findB.setEnabled(false);
+					itemB.setEnabled(true);
+					customerInfo.setText("Customer Selected\n"+cTemp);
+				}
+			}
+		});
+		
+		findB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				if (hpno.keySet().contains(Integer.parseInt(phone.getText()))) {
+					JOptionPane.showMessageDialog(null, "Customer Found", "Detail Exists", JOptionPane.INFORMATION_MESSAGE);
+					Customer cTemp = hpno.get(Integer.parseInt(phone.getText()));
+					phone.setEnabled(false);
+					addB.setEnabled(false);
+					findB.setEnabled(false);
+					itemB.setEnabled(true);
+					customerInfo.setText("Customer Selected\n"+cTemp);
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Customer Doesn't Exists", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 		
 	}
 }
